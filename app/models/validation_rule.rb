c98fa5fdce26a5749b3fa4dc99ae005a69bd7821
Mwatha Bwanali <mwathabwanali@gmail.com>
2014-01-29 17:15:49 -0500

    adding supervision tools: 5%

--- app/models/validation_rule.rb
+++ app/models/validation_rule.rb
@@ -1,2 +1,19 @@
 class ValidationRule < ActiveRecord::Base
+
+
+
+
+
+  #........................user story 2 (Supervision tools)....................
+
+  def self.data_consistency_checks
+  end
+
+
+  private
+
+  def dead_patients_with_visits
+    
+  end
+
 end
